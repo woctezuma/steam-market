e3ecd1cc8e6c51955d1e3b6924fe7c353edc1698
Wok <woctezuma@users.noreply.github.com>
2019-04-27 16:29:57 +0200

    Manual fit of the fee cost for prices less than or equal to 0.66€ (until I figure out the proper formula and roundings used by Valve)
    Reference:
    https://steamcommunity.com/discussions/forum/1/1679190184065722423/

--- transaction_fee.py
+++ transaction_fee.py
@@ -14,6 +14,31 @@ def get_game_specific_transaction_fee():
     return game_specific_transaction_fee
 
 
+def get_ground_truth_sell_price_without_fee(sell_price_including_fee):
+    # Reference: https://steamcommunity.com/discussions/forum/1/1679190184065722423/
+
+    # Manual fit of the fee cost for small prices (less than or equal to 0.66€)
+    if sell_price_including_fee <= 0.21:
+        total_fee_price = 0.02
+    elif sell_price_including_fee <= 0.32:
+        total_fee_price = 0.03
+    elif sell_price_including_fee <= 0.43:
+        total_fee_price = 0.04
+    elif sell_price_including_fee == 0.44:
+        total_fee_price = 0.05
+    elif sell_price_including_fee <= 0.55:
+        total_fee_price = 0.06
+    elif sell_price_including_fee <= 0.66:
+        total_fee_price = 0.07
+    else:
+        raise AssertionError()
+
+    sell_price_without_fee = sell_price_including_fee - total_fee_price
+    sell_price_without_fee = float('{0:.2f}'.format(sell_price_without_fee))
+
+    return sell_price_without_fee
+
+
 def compute_sell_price_without_fee(sell_price_including_fee):
     # Caveat: this is not exact! The price without fee can be off by 1 cent!
 
@@ -45,6 +70,10 @@ def compute_sell_price_without_fee(sell_price_including_fee):
 
     sell_price_without_fee = sell_price_including_fee - total_fee_price
     sell_price_without_fee = float('{0:.2f}'.format(sell_price_without_fee))
+
+    # Manually adjust the fee cost for small prices (until I have access to the right formula)
+    if sell_price_including_fee <= 0.66:
+        sell_price_without_fee = get_ground_truth_sell_price_without_fee(sell_price_including_fee)
 
     return sell_price_without_fee
 
