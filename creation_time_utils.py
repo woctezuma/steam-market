1a451ac84565897b53fb184e05a01adf754b734a
Wok <woctezuma@users.noreply.github.com>
2019-09-19 18:45:45 +0200

    Simplify code: intersection of dictionary keys for the creation times

--- creation_time_utils.py
+++ creation_time_utils.py
@@ -27,34 +27,32 @@ def fill_in_badges_with_next_creation_times_loaded_from_disk(aggregated_badge_da
                                                              verbose=True):
     next_creation_times_loaded_from_disk = load_next_creation_time_data()
 
-    for app_id in aggregated_badge_data.keys():
-        try:
-            next_creation_time = next_creation_times_loaded_from_disk[app_id]
-        except KeyError:
-            next_creation_time = None
+    app_ids = set(aggregated_badge_data.keys()).intersection(next_creation_times_loaded_from_disk.keys())
 
-        if next_creation_time is not None:
-            previously_loaded_next_creation_time = aggregated_badge_data[app_id]['next_creation_time']
-            aggregated_badge_data[app_id]['next_creation_time'] = next_creation_time
+    for app_id in app_ids:
+        next_creation_time = next_creation_times_loaded_from_disk[app_id]
 
-            if verbose:
-                app_name = aggregated_badge_data[app_id]['name']
-                if previously_loaded_next_creation_time is None:
-                    print('Loading the next creation time ({}) for {} (appID = {}) from disk.'.format(
-                        next_creation_time,
-                        app_name,
-                        app_id))
-                else:
-                    # NB: Data stored in data/next_creation_times.json is assumed to be more up-to-date compared to
-                    #     data stored in data/booster_game_creator_from_javascript.txt. Indeed, if you  update the .txt
-                    #     file with data found on your Booster Pack Creator page, then the .json file would be useless,
-                    #     and you should delete it. Therefore, if the .json file is present on your disk, it can be
-                    #     assumed that it was created by running this program, thus is more recent than the .txt file.
-                    print('Replacing the next creation time ({}) for {} (appID = {}) with {}, loaded from disk.'.format(
-                        previously_loaded_next_creation_time,
-                        app_name,
-                        app_id,
-                        next_creation_time))
+        previously_loaded_next_creation_time = aggregated_badge_data[app_id]['next_creation_time']
+        aggregated_badge_data[app_id]['next_creation_time'] = next_creation_time
+
+        if verbose:
+            app_name = aggregated_badge_data[app_id]['name']
+            if previously_loaded_next_creation_time is None:
+                print('Loading the next creation time ({}) for {} (appID = {}) from disk.'.format(
+                    next_creation_time,
+                    app_name,
+                    app_id))
+            else:
+                # NB: Data stored in data/next_creation_times.json is assumed to be more up-to-date compared to
+                #     data stored in data/booster_game_creator_from_javascript.txt. Indeed, if you  update the .txt
+                #     file with data found on your Booster Pack Creator page, then the .json file would be useless,
+                #     and you should delete it. Therefore, if the .json file is present on your disk, it can be
+                #     assumed that it was created by running this program, thus is more recent than the .txt file.
+                print('Replacing the next creation time ({}) for {} (appID = {}) with {}, loaded from disk.'.format(
+                    previously_loaded_next_creation_time,
+                    app_name,
+                    app_id,
+                    next_creation_time))
 
     return aggregated_badge_data
 
