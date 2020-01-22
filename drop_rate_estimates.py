9ea4291ef45e3a817541065f1b835e76ea78386c
Wok <woctezuma@users.noreply.github.com>
2020-01-23 00:14:03 +0100

    More generic code for drop-rates

--- drop_rate_estimates.py
+++ drop_rate_estimates.py
@@ -6,35 +6,52 @@
 # NB²: we assume that the drop-rates are identical for every games. However, I have suspicions that the drop-rates may
 # vary based on the number of items of each rarity.
 
+from utils import get_category_name_for_profile_backgrounds, get_category_name_for_emoticons
+
+
+def get_drop_rate_field():
+    drop_rate_field = 'drop_rate'
+
+    return drop_rate_field
+
+
 def get_drop_rate_estimates(verbose=True):
     drop_rate_estimates = dict()
 
     drop_rate_estimates['badges'] = 236
 
-    drop_rate_estimates['profile_backgrounds'] = dict()
-    drop_rate_estimates['profile_backgrounds']['drop_rate'] = dict()
-    drop_rate_estimates['profile_backgrounds']['drop_rate']['common'] = 0.6271
-    drop_rate_estimates['profile_backgrounds']['drop_rate']['uncommon'] = 0.2585
-    drop_rate_estimates['profile_backgrounds']['drop_rate']['rare'] = 0.1144
+    category_field = get_category_name_for_profile_backgrounds()
+    drop_rate_field = get_drop_rate_field()
 
-    drop_rate_estimates['emoticons'] = dict()
-    drop_rate_estimates['emoticons']['drop_rate'] = dict()
-    drop_rate_estimates['emoticons']['drop_rate']['common'] = 0.7458
-    drop_rate_estimates['emoticons']['drop_rate']['uncommon'] = 0.2034
-    drop_rate_estimates['emoticons']['drop_rate']['rare'] = 0.0508
+    drop_rate_estimates[category_field] = dict()
+    drop_rate_estimates[category_field][drop_rate_field] = dict()
+    drop_rate_estimates[category_field][drop_rate_field]['common'] = 0.6271
+    drop_rate_estimates[category_field][drop_rate_field]['uncommon'] = 0.2585
+    drop_rate_estimates[category_field][drop_rate_field]['rare'] = 0.1144
+
+    category_field = get_category_name_for_emoticons()
+
+    drop_rate_estimates[category_field] = dict()
+    drop_rate_estimates[category_field][drop_rate_field] = dict()
+    drop_rate_estimates[category_field][drop_rate_field]['common'] = 0.7458
+    drop_rate_estimates[category_field][drop_rate_field]['uncommon'] = 0.2034
+    drop_rate_estimates[category_field][drop_rate_field]['rare'] = 0.0508
 
     if verbose:
         print('Drop-rate estimates after crafting {} badges:'.format(
             drop_rate_estimates['badges']
         ))
 
-        for field in ['profile_backgrounds', 'emoticons']:
+        for category_field in [
+            get_category_name_for_profile_backgrounds(),
+            get_category_name_for_emoticons(),
+        ]:
             print('- {}:\n\t{:.2f} (Common), {:.2f} (Uncommon), {:.2f} (Rare) ; sum = {:.2f}'.format(
-                field,
-                drop_rate_estimates[field]['drop_rate']['common'],
-                drop_rate_estimates[field]['drop_rate']['uncommon'],
-                drop_rate_estimates[field]['drop_rate']['rare'],
-                sum(p for p in drop_rate_estimates[field]['drop_rate'].values())
+                category_field,
+                drop_rate_estimates[category_field][drop_rate_field]['common'],
+                drop_rate_estimates[category_field][drop_rate_field]['uncommon'],
+                drop_rate_estimates[category_field][drop_rate_field]['rare'],
+                sum(p for p in drop_rate_estimates[category_field][drop_rate_field].values())
             ))
 
     return drop_rate_estimates
