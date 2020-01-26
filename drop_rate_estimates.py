8725dc2b0c1f019c8ec8fefcdaae14c42188e65e
Wok <woctezuma@users.noreply.github.com>
2020-01-26 22:47:42 +0100

    Drop-rate estimates conditionally on the item rarity pattern C/UC/R (the numbers of possible items of each rarity)

--- drop_rate_estimates.py
+++ drop_rate_estimates.py
@@ -28,7 +28,64 @@ def clamp_proportion(input_proportion):
     return clampped_proportion
 
 
+def get_drop_rate_estimates_based_on_item_rarity_pattern(verbose=True):
+    # Drop-rate estimates conditionally on the item rarity pattern C/UC/R (the numbers of possible items of each rarity)
+
+    drop_rate_estimates = dict()
+
+    drop_rate_estimates['badges'] = 239
+
+    drop_rate_field = get_drop_rate_field()
+    rarity_field = 'common'
+
+    drop_rate_estimates[drop_rate_field] = dict()
+
+    # Drop rates for common rarity based on the item rarity pattern (C, UC, R):
+    drop_rate_estimates[drop_rate_field][rarity_field] = {
+        (1, 1, 1): 0.6294,
+        (1, 2, 1): 0.3832,
+        (1, 2, 2): 0.5560,
+        (2, 1, 1): 0.5916,
+        (2, 2, 1): 0.5060,
+        (2, 2, 2): 0.5722,
+        (2, 2, 5): 0.5000,
+        (2, 3, 1): 0.5566,
+        (2, 5, 2): 0.3989,
+        (3, 1, 1): 0.7911,
+        (3, 2, 1): 0.7726,
+        (4, 3, 2): 0.5413,
+        (5, 1, 1): 0.6676,
+        (3, 4, 3): 0.5337,
+        (4, 1, 1): 0.7726,
+        (4, 2, 1): 0.7828,
+    }
+
+    common_drop_rate = drop_rate_estimates[drop_rate_field][rarity_field]
+
+    for pattern in common_drop_rate:
+        current_drop_rate = common_drop_rate[pattern]
+        drop_rate_estimates[drop_rate_field][rarity_field][pattern] = clamp_proportion(current_drop_rate)
+
+    if verbose:
+        print('Drop-rate estimates after crafting {} badges:'.format(
+            drop_rate_estimates['badges']
+        ))
+
+        common_drop_rate = drop_rate_estimates[drop_rate_field][rarity_field]
+
+        for pattern in common_drop_rate:
+            print('- C/UC/R: {}\t--->\t{:.2f} ({})'.format(
+                pattern,
+                common_drop_rate[pattern],
+                rarity_field.capitalize()
+            ))
+
+    return drop_rate_estimates
+
+
 def get_drop_rate_estimates(verbose=True):
+    # Drop-rate estimates conditionally on the category (profile backgrounds, emoticons)
+
     drop_rate_estimates = dict()
 
     drop_rate_estimates['badges'] = 236
@@ -81,6 +138,8 @@ def get_drop_rate_estimates(verbose=True):
 
 def main():
     drop_rate_estimates = get_drop_rate_estimates(verbose=True)
+
+    drop_rate_estimates = get_drop_rate_estimates_based_on_item_rarity_pattern(verbose=True)
 
     return True
 
