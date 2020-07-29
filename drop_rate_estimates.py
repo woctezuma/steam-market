d69ce5361779ceeaceef11e66c8a30cb09c546da
Wok <woctezuma@users.noreply.github.com>
2020-07-29 18:32:48 +0200

    Data: update drop-rate estimates

--- drop_rate_estimates.py
+++ drop_rate_estimates.py
@@ -39,7 +39,7 @@ def get_drop_rate_estimates_based_on_item_rarity_pattern(verbose=True):
 
     drop_rate_estimates = dict()
 
-    drop_rate_estimates['badges'] = 451
+    drop_rate_estimates['badges'] = 777
 
     drop_rate_field = get_drop_rate_field()
     badge_count_field = get_badge_count_field()
@@ -54,27 +54,32 @@ def get_drop_rate_estimates_based_on_item_rarity_pattern(verbose=True):
     # Reference: https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Wilson_score_interval
     drop_rate_estimates[drop_rate_field][rarity_field] = {
         # Patterns found for profile background and emoticons:
-        (1, 1, 1): 0.5941,
-        (2, 2, 1): 0.5790,
+        (1, 1, 1): 0.5516,
+        (2, 2, 1): 0.6160,
         (2, 3, 1): 0.5796,
-        (3, 1, 1): 0.8036,
+        (3, 1, 1): 0.8072,
+        (3, 2, 1): 0.6102,
         (3, 4, 3): 0.6135,
         (4, 3, 2): 0.6384,
-        (5, 1, 1): 0.7242,
+        (5, 1, 1): 0.7299,
         # Patterns only found for profile background:
+        (1, 1, 2): 0.5891,
         (1, 2, 1): 0.3832,
         (1, 2, 2): 0.5718,
-        (2, 1, 1): 0.6493,
-        (2, 2, 2): 0.6200,
-        (2, 2, 5): 0.3989,
+        (2, 1, 1): 0.7236,
+        (2, 1, 3): 0.4434,
+        (2, 2, 2): 0.6678,
+        (2, 2, 5): 0.3673,
         (2, 5, 2): 0.3989,
-        (3, 2, 1): 0.7097,
-        (4, 2, 2): 0.7306,
+        (3, 2, 1): 0.6102,
+        (4, 2, 2): 0.6894,
         # Patterns only found for emoticons:
-        (4, 1, 1): 0.7517,
-        (4, 2, 1): 0.7828,
+        (2, 1, 2): 0.4161,
+        (3, 3, 1): 0.4369,
+        (4, 1, 1): 0.7617,
+        (4, 2, 1): 0.8019,
         (4, 3, 3): 0.3742,
-        (5, 2, 2): 0.5461,
+        (5, 2, 2): 0.5000,
         (6, 2, 2): 0.8024,
         (8, 1, 1): 0.7828,
     }
@@ -87,27 +92,32 @@ def get_drop_rate_estimates_based_on_item_rarity_pattern(verbose=True):
 
     drop_rate_estimates[badge_count_field][rarity_field] = {
         # Patterns found for profile background and emoticons:
-        (1, 1, 1): 129,
-        (2, 2, 1): 129,
+        (1, 1, 1): 190,
+        (2, 2, 1): 298,
         (2, 3, 1): 15,
-        (3, 1, 1): 347,
+        (3, 1, 1): 639,
+        (3, 2, 1): 37,
         (3, 4, 3): 27,
         (4, 3, 2): 7,
-        (5, 1, 1): 43,
+        (5, 1, 1): 44,
         # Patterns only found for profile background:
+        (1, 1, 2): 13,
         (1, 2, 1): 9,
         (1, 2, 2): 24,
-        (2, 1, 1): 33,
-        (2, 2, 2): 17,
-        (2, 2, 5): 11,
+        (2, 1, 1): 61,
+        (2, 1, 3): 5,
+        (2, 2, 2): 20,
+        (2, 2, 5): 15,
         (2, 5, 2): 11,
-        (3, 2, 1): 20,
-        (4, 2, 2): 7,
+        (3, 2, 1): 37,
+        (4, 2, 2): 12,
         # Patterns only found for emoticons:
-        (4, 1, 1): 20,
-        (4, 2, 1): 5,
+        (2, 1, 2): 20,
+        (3, 3, 1): 12,
+        (4, 1, 1): 21,
+        (4, 2, 1): 21,
         (4, 3, 3): 20,
-        (5, 2, 2): 7,
+        (5, 2, 2): 12,
         (6, 2, 2): 16,
         (8, 1, 1): 5,
     }
@@ -143,7 +153,7 @@ def get_drop_rate_estimates(verbose=True):
 
     drop_rate_estimates = dict()
 
-    drop_rate_estimates['badges'] = 451
+    drop_rate_estimates['badges'] = 777
 
     category_field = get_category_name_for_profile_backgrounds()
     drop_rate_field = get_drop_rate_field()
@@ -151,9 +161,9 @@ def get_drop_rate_estimates(verbose=True):
 
     drop_rate_estimates[category_field] = dict()
     drop_rate_estimates[category_field][drop_rate_field] = dict()
-    drop_rate_estimates[category_field][drop_rate_field]['common'] = 0.6475
-    drop_rate_estimates[category_field][drop_rate_field]['uncommon'] = 0.2373
-    drop_rate_estimates[category_field][drop_rate_field]['rare'] = 0.1153
+    drop_rate_estimates[category_field][drop_rate_field]['common'] = 0.6607
+    drop_rate_estimates[category_field][drop_rate_field]['uncommon'] = 0.2202
+    drop_rate_estimates[category_field][drop_rate_field]['rare'] = 0.1216
 
     for rarity in rarity_fields:
         current_drop_rate = drop_rate_estimates[category_field][drop_rate_field][rarity]
@@ -163,9 +173,9 @@ def get_drop_rate_estimates(verbose=True):
 
     drop_rate_estimates[category_field] = dict()
     drop_rate_estimates[category_field][drop_rate_field] = dict()
-    drop_rate_estimates[category_field][drop_rate_field]['common'] = 0.7384
-    drop_rate_estimates[category_field][drop_rate_field]['uncommon'] = 0.1840
-    drop_rate_estimates[category_field][drop_rate_field]['rare'] = 0.0776
+    drop_rate_estimates[category_field][drop_rate_field]['common'] = 0.7235
+    drop_rate_estimates[category_field][drop_rate_field]['uncommon'] = 0.1946
+    drop_rate_estimates[category_field][drop_rate_field]['rare'] = 0.0844
 
     for rarity in rarity_fields:
         current_drop_rate = drop_rate_estimates[category_field][drop_rate_field][rarity]
