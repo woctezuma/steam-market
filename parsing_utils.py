297b1bb7c51d53dc10cc67ad98ce02aa5aef08ee
LeLuCh B0й <9300326+alekxeyuk@users.noreply.github.com>
2022-05-25 15:31:11 +0700

    Using the built-in module for json parsing
    The JavaScript code from the steam page is actually a JSON array, moreover, it is also valid python code.
    Thus, there is no need to perform manual parsing/correction of unicode characters.
    
    I've reworked the parsing to use the built-in json module, used f-string instead of format, removed obsolete functions.

--- parsing_utils.py
+++ parsing_utils.py
@@ -4,60 +4,11 @@
 # - either on a previous manual copy-paste of HTML code to data/booster_game_creator.txt
 # - or on a previous manual copy-paste of javascript code to data/booster_game_creator_from_javascript.txt
 
+import json
 from pathlib import Path
 
 from market_listing import fix_app_name_for_url_query
 from utils import get_badge_creation_file_name
-
-
-def fix_unicode_characters_in_app_name_from_javascript_code(app_name: str) -> str:
-    app_name = app_name.replace('\\u00ae', '®')
-    app_name = app_name.replace('\\u00db', 'Û')
-    app_name = app_name.replace('\\u00e9', 'é')
-    app_name = app_name.replace('\\u00fc', 'ü')
-    app_name = app_name.replace('\\u2019', '’')
-    app_name = app_name.replace('\\u2122', '™')
-    app_name = app_name.replace('\\u4e1c', '东')
-    app_name = app_name.replace('\\u4e89', '争')
-    app_name = app_name.replace('\\u4e94', '五')
-    app_name = app_name.replace('\\u5927', '大')
-    app_name = app_name.replace('\\u5e08', '师')
-    app_name = app_name.replace('\\u6218', '战')
-    app_name = app_name.replace('\\u62ef', '拯')
-    app_name = app_name.replace('\\u6551', '救')
-    app_name = app_name.replace('\\u65b9', '方')
-    app_name = app_name.replace('\\u666f', '景')
-    app_name = app_name.replace('\\u708e', '炎')
-    app_name = app_name.replace('\\u7248', '版')
-    app_name = app_name.replace('\\u738b', '王')
-    app_name = app_name.replace('\\u795e', '神')
-    app_name = app_name.replace('\\u7d76', '絶')
-    app_name = app_name.replace('\\u7eaa', '纪')
-    app_name = app_name.replace('\\u884c', '行')
-    app_name = app_name.replace('\\u9b54', '魔')
-    app_name = app_name.replace('\\u9ec4', '黄')
-
-    return app_name
-
-
-def get_sub_string(input_str: str, key_start_str: str, key_end_str: str = None) -> str:
-    try:
-        search_start_index = input_str.index(key_start_str)
-        sub_string_start_index = search_start_index + len(key_start_str)
-        sub_string = input_str[sub_string_start_index:]
-    except ValueError:
-        sub_string = ''
-
-    try:
-        search_end_index = sub_string.index(key_end_str)
-        sub_string_end_index = search_end_index
-        sub_string = sub_string[:sub_string_end_index]
-    except ValueError:
-        pass
-    except TypeError:
-        pass
-
-    return sub_string
 
 
 def parse_javascript_one_liner(badges_as_str: str, verbose: bool = False) -> dict[int, dict]:
@@ -65,49 +16,22 @@ def parse_javascript_one_liner(badges_as_str: str, verbose: bool = False) -> dic
 
     print('Parsing the one-line javascript code displayed with the web browser.')
 
-    # Strip the start
-    badges_as_str = badges_as_str.strip('[{')
-    # Strip the end
-    badges_as_str = badges_as_str.strip('}],')
-    # Split into tokens
-    badges_as_list = badges_as_str.split('},{')
+    badges = json.loads(badges_as_str.lstrip().rstrip(','))
+    for badge in badges:
+        app_id, next_creation_time = badge['appid'], "available now"
+        badge_creation_details[app_id] = {
+            'name': (app_name := badge['name']),
+            'gem_value': (gem_value := int(badge['price']))
+        }
 
-    for badge_as_str in badges_as_list:
+        if badge.get('unavailable', False):
+            next_creation_time = badge['available_at_time']
+            badge_creation_details[app_id]['next_creation_time'] = next_creation_time
 
-        field_separator = ','
-
-        app_id = get_sub_string(badge_as_str, '"appid":', field_separator + '"name":')
-        app_name = get_sub_string(badge_as_str, '"name":', field_separator + '"series":')
-        gem_value = get_sub_string(badge_as_str, '"price":', field_separator + '"unavailable":')
-        next_creation_time = get_sub_string(badge_as_str, '"available_at_time":')
-
-        app_id = int(app_id)
-        app_name = fix_unicode_characters_in_app_name_from_javascript_code(app_name.strip('"'))
-        try:
-            gem_value = int(gem_value.strip('"'))
-        except ValueError:
-            # For the last game entry on the line, there is a remaining '}' character
-            gem_value = gem_value.split('}')[0]
-            gem_value = int(gem_value.strip('"'))
-
-        badge_creation_details[app_id] = dict()
-        badge_creation_details[app_id]['name'] = app_name
-        badge_creation_details[app_id]['gem_value'] = gem_value
-        if len(next_creation_time) > 0:
-            next_creation_time_inner_str = next_creation_time[1:-1]
-            badge_creation_details[app_id]['next_creation_time'] = next_creation_time_inner_str
-
-            print('Loading the next creation time ({}) for {} (appID = {}) from the Booster Pack Creator list.'.format(
-                next_creation_time_inner_str,
-                app_name,
-                app_id))
+            print(f'Loading the next creation time ({next_creation_time}) for {app_name} ({app_id = }) from the Booster Pack Creator list.')
 
         if verbose:
-            print('{}\t{}\t{} gems\t{}'.format(app_id, app_name, gem_value, next_creation_time))
-
-        if '\\u' in app_name:
-            # Manually add the characters to replace in fix_unicode_characters_in_app_name_from_javascript_code()
-            print('[warning] unicode characters not interpreted correctly in:\t{}'.format(app_name))
+            print(f'{app_id:<10}{app_name}\t{gem_value} gems\t{next_creation_time}')
 
     return badge_creation_details
 
