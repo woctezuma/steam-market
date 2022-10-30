a349b129d20d5bb08866c1ed6f655929a8860890
Wok <woctezuma@users.noreply.github.com>
2022-10-30 10:31:11 +0100

    Print the error reason when downloading market order data for gems

--- sack_of_gems.py
+++ sack_of_gems.py
@@ -20,7 +20,7 @@ def get_num_gems_per_sack_of_gems() -> int:
     return num_gems_per_sack_of_gems
 
 
-def download_sack_of_gems_price(sack_of_gems_listing_file_name: str = None) -> float:
+def download_sack_of_gems_price(sack_of_gems_listing_file_name: str = None, verbose: bool = True) -> float:
     if sack_of_gems_listing_file_name is None:
         sack_of_gems_listing_file_name = get_sack_of_gems_listing_file_name()
 
@@ -35,7 +35,7 @@ def download_sack_of_gems_price(sack_of_gems_listing_file_name: str = None) -> f
     if status_code == 200:
         item_nameid = listing_details[listing_hash]['item_nameid']
 
-        bid_price, ask_price, bid_volume, ask_volume = download_market_order_data(listing_hash, item_nameid)
+        bid_price, ask_price, bid_volume, ask_volume = download_market_order_data(listing_hash, item_nameid, verbose)
         listing_details[listing_hash]['bid'] = bid_price
         listing_details[listing_hash]['ask'] = ask_price
         listing_details[listing_hash]['bid_volume'] = bid_volume
