42f46252d73fa18004d533aaaa0047acadffa674
Wok <woctezuma@users.noreply.github.com>
2024-11-25 23:38:06 +0100

    Use static method in tests

--- tests.py
+++ tests.py
@@ -17,7 +17,8 @@ from src import (
 
 
 class TestMarketListingMethods(unittest.TestCase):
-    def test_get_listing_details_batch(self) -> None:
+    @staticmethod
+    def test_get_listing_details_batch() -> None:
         listing_hashes = [
             "407420-Gabe Newell Simulator Booster Pack",
             "443380-Tokyo Babel Booster Pack",
@@ -31,46 +32,54 @@ class TestMarketListingMethods(unittest.TestCase):
 
         assert len(all_listing_details) == len(listing_hashes)
 
-    def test_main(self) -> None:
+    @staticmethod
+    def test_main() -> None:
         assert market_listing.main() is True
 
 
 class TestParsingUtilsMethods(unittest.TestCase):
-    def test_main(self) -> None:
+    @staticmethod
+    def test_main() -> None:
         assert parsing_utils.main() is True
 
 
 class TestCreationTimeUtilsMethods(unittest.TestCase):
-    def test_main(self) -> None:
+    @staticmethod
+    def test_main() -> None:
         assert creation_time_utils.main() is True
 
 
 class TestSackOfGemsMethods(unittest.TestCase):
-    def test_download_sack_of_gems_price(self) -> None:
+    @staticmethod
+    def test_download_sack_of_gems_price() -> None:
         sack_of_gems_price = sack_of_gems.download_sack_of_gems_price()
 
         assert sack_of_gems_price > 0
 
-    def test_load_sack_of_gems_price(self) -> None:
+    @staticmethod
+    def test_load_sack_of_gems_price() -> None:
         sack_of_gems_price = sack_of_gems.load_sack_of_gems_price()
 
         assert sack_of_gems_price > 0
 
 
 class TestMarketSearchMethods(unittest.TestCase):
-    def test_download_all_listings(self) -> None:
+    @staticmethod
+    def test_download_all_listings() -> None:
         assert market_search.download_all_listings() is True
 
 
 class TestMarketUtilsMethods(unittest.TestCase):
-    def test_load_aggregated_badge_data(self) -> None:
+    @staticmethod
+    def test_load_aggregated_badge_data() -> None:
         aggregated_badge_data = market_utils.load_aggregated_badge_data()
 
         assert aggregated_badge_data
 
 
 class TestMarketArbitrageMethods(unittest.TestCase):
-    def test_apply_workflow(self) -> None:
+    @staticmethod
+    def test_apply_workflow() -> None:
         try:
             flag = market_arbitrage.apply_workflow(
                 retrieve_listings_from_scratch=False,
@@ -84,7 +93,8 @@ class TestMarketArbitrageMethods(unittest.TestCase):
 
 
 class TestMarketOrderMethods(unittest.TestCase):
-    def test_main(self) -> None:
+    @staticmethod
+    def test_main() -> None:
         try:
             flag = market_order.main()
         except KeyError:
@@ -95,22 +105,26 @@ class TestMarketOrderMethods(unittest.TestCase):
 
 
 class TestUtilsMethods(unittest.TestCase):
-    def test_main(self) -> None:
+    @staticmethod
+    def test_main() -> None:
         assert utils.main() is True
 
 
 class TestTransactionFeeMethods(unittest.TestCase):
-    def test_main(self) -> None:
+    @staticmethod
+    def test_main() -> None:
         assert transaction_fee.main() is True
 
 
 class TestBatchCreatePacksMethods(unittest.TestCase):
-    def test_main(self) -> None:
+    @staticmethod
+    def test_main() -> None:
         assert batch_create_packs.main(is_a_simulation=True) is True
 
 
 class TestDropRateEstimatesMethods(unittest.TestCase):
-    def test_main(self) -> None:
+    @staticmethod
+    def test_main() -> None:
         assert drop_rate_estimates.main() is True
 
 
